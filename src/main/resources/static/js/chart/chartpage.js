6307324d2601e6baab923301759c560bb6691b22
samdae3@naver.com <samdae3@naver.com>
2021-04-18 16:50:33 +0900

    디비에서 끄내는걸로

--- src/main/resources/static/js/chart/chartpage.js
+++ src/main/resources/static/js/chart/chartpage.js
@@ -9,72 +9,33 @@ function getChart() {
         method: 'get',
         dataType: 'json',
         success: function(data) {
-            console.log(data);
-        }
-    });
-    $.ajax({
-        url: '/melon',
-        dataType: 'json',
-        success: function(data) {
-            listSet('melon', data.melon);
-        }
-    });
-    $.ajax({
-        url: '/youtube',
-        dataType: 'json',
-        success: function(data) {
-            listSet('youtube', data.youtube);
-        }
-    });
-    $.ajax({
-        url: '/bugs',
-        dataType: 'json',
-        success: function(data) {
-            listSet('bugs', data.bugs);
-        }
-    });
-    $.ajax({
-        url: '/genie',
-        dataType: 'json',
-        success: function(data) {
-            listSet('genie', data.genie);
-        }
-    });
-    $.ajax({
-        url: '/flo',
-        dataType: 'json',
-        success: function(data) {
-            listSet('flo', data.flo);
-        }
-    });
-    $.ajax({
-        url: '/vibe',
-        dataType: 'json',
-        success: function(data) {
-            listSet('vibe', data.vibe);
+            console.log(data.data);
+            listSet(data.data);
         }
     });
 }
 
-function listSet(id, data) {
-    var tbody = $("#"+id);
-    tbody.empty();
-    var html = '';
+function listSet(data) {
+    $("#melon").empty();
+    $("#bugs").empty();
+    $("#genie").empty();
+    $("#flo").empty();
+    $("#vibe").empty();
+    $("#youtube").empty();
+
     for (var i=0; i<data.length; i++) {
+        var html = '';
         var rank = data[i].ranking;
         var title = data[i].title;
-        // var name = data[i].name;
-        // var img = data[i].img;
         html += '<tr>' +
             '<td class="rank" style="text-align: center">' +  rank + '</td>' +
-            '<td class="tdImg">' +
-            '<img class="rankImg"src="../../chart-image/' + _img(title) + '" />' +
-            '</td><td>'+title+'</td></tr>';
+            '<td class="tdImg"><img class="rankImg"src="../../chart-image/' + _img(title) + '" /></td>' +
+            '<td>'+title+'</td></tr>';
+        $("#"+data[i].site).append(html);
     }
-    tbody.append(html);
 }
 function _img(title){
-    if( title.indexOf("(Rollin')")     > -1 ) return "x74e-afep934nbksq.jpg";
+    if( title.indexOf("(Rollin`)")     > -1 ) return "x74e-afep934nbksq.jpg";
     if( title.indexOf("(We Ride)")     > -1 ) return "x31j-fjf329bn20gnv.jpg";
     if( title.indexOf("하이힐")          > -1 ) return "x87m-ajfie319nmg9.jpg";
     if( title.indexOf("(New Version)") > -1 ) return "x67e-qnvj931opqwb.jpg";
